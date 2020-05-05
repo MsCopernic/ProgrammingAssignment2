ccce0ff8029aa690191ea895262b5c61a57e19c4
Jorge Perez <jlpdelat@me.com>
2020-05-05 11:40:29 -0500

    Source code for the Pogramming Assignment 2: Lexical Scoping from Coursera R Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first two functions, create a special object, that
+## stores a matrix and then caches its inverse
 
-## Write a short comment describing this function
+## MakeCacheMatrix, creates a special matrix object
 
 makeCacheMatrix <- function(x = matrix()) {
+   
+   i <- NULL
+   
+   set <- function(y) {
+      x <<- y
+      i <<- NULL
+   }
+   
+   get <- function() x
+   setinverse <- function(inverse) i <<- inverse
+   getinverse <- function() i
+   
+   list(set = set,
+        get = get,
+        setinverse = setinverse,
+        getinverse = getinverse)
+}
+
+## caheSolve, produces the inverse of the special matrix
+## created by MakeCacheMatrix
+
+cacheSolve <- function(x, ...) {
+   
+   i <- x$getinverse()
+   
+   if (!is.null(i)) {
+      message("getting data from cache")
+      return(i)
+   }
+
+   data <- x$get()
+   i <- solve(data, ...)
+   x$setinverse(i)
+   i
 
 }
 
+## Source this program and call run this function is
+## just a sample "keyboard shortcut", but you may try
+## MakeCacheMatrix & cacheSolve with your own data
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+run <- function() {
+   
+   x <- matrix( rnorm(20), 4,4)
+   y <- makeCacheMatrix( x )
+   cacheSolve( y )
+   
 }
